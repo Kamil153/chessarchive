998a581406868e0382393971c95b9812592fbc85
dmncp <damiancyper@gmail.com>
2021-05-10 20:45:28 +0200

    signup system

--- chessarchive/urls.py
+++ chessarchive/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
 from django.views.generic.base import TemplateView
+from archive import views as v
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('accounts/', include('archive.urls')),
 ]
