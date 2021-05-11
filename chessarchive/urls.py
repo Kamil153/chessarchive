77789be7f26de235069b6629893d2f113ed0abc2
Kamil153 <kamilpolak15@gmail.com>
2021-05-11 23:09:04 +0200

    remove login_required

--- chessarchive/urls.py
+++ chessarchive/urls.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.contrib.auth.decorators import login_required
 from django.urls import path, include
 
 from archive.views import SignUpView
@@ -7,6 +6,6 @@ from archive.views import SignUpView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', login_required(include('archive.urls'))),
+    path('', include('archive.urls')),
     path('signup/', SignUpView.as_view(), name='signup'),
 ]
