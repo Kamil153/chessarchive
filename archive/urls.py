85bea9b3438ef94034f034f286c2af515043e8a2
Kamil153 <kamilpolak15@gmail.com>
2021-05-11 23:07:31 +0200

    require login to access archive

--- archive/urls.py
+++ archive/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-
-from .views import SignUpView
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup'),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
--- chessarchive/urls.py
+++ chessarchive/urls.py
@@ -1,27 +1,12 @@
-"""chessarchive URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth.decorators import login_required
 from django.urls import path, include
-from django.views.generic.base import TemplateView
-from archive import views as v
+
+from archive.views import SignUpView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('accounts/', include('archive.urls')),
+    path('', login_required(include('archive.urls'))),
+    path('signup/', SignUpView.as_view(), name='signup'),
 ]
